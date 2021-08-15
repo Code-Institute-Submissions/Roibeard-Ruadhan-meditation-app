8249a4ece08f4eabb0968f54ce8f12d0edf98fa6
Roibéard R Coughlan <roibeard.c@outlook.com>
2021-08-15 16:11:02 +0000

    Get outLineLength by using declaring outLineLength = outline.getTotalLength(). Comment to explain code & use console.log to confirm outLineLength = 1359.7597

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,14 @@ const app = () => {
     const outline = document.querySelector('.moving-outline circle');
     const video = document.querySelector('.vid-container video')
 
+    // Select all the Sounds using querySelectorAll
+    const sounds = document.querySelectorAll('.sound-picker button');
+    // Time display
+    const timeDisplay = document.querySelector('.time-display');
+    // Get the lenght of the outline for the sounds to allow annimation
+    const outlineLength = outline.getTotalLength();
+    console.log(outlineLength);
+
 }
 
 app();
