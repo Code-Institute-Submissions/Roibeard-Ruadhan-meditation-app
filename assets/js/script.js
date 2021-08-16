020239e796d70c498e42801585b0fe5e156151a5
Roibéard R Coughlan <roibeard.c@outlook.com>
2021-08-16 12:33:26 +0000

    Add video.pause() under the if section when the fakeDuration ends after testing

--- assets/js/script.js
+++ assets/js/script.js
@@ -44,39 +44,39 @@ timeSelect.forEach(option => {
 });
 
 // Create a function specific to stop and play the sounds
-        const checkPlaying = song => {
-            if(song.paused) {
-                song.play();
-                video.play();
-                play.src = '/assets/svg/pause.svg';
-            }else{
-                song.pause();
-                video.pause();
-                play.src = '/assets/svg/play.svg';
-            }
-        };
+const checkPlaying = song => {
+    if(song.paused) {
+        song.play();
+        video.play();
+        play.src = '/assets/svg/pause.svg';
+    }else{
+        song.pause();
+        video.pause();
+        play.src = '/assets/svg/play.svg';
+    }
+};
 
-    //Preparations to animate the circle, to update when the song plays
-    song.ontimeupdate = () => {
-        let currentTime = song.currentTime;
-        let elapsed = fakeDuration - currentTime;
-        let seconds = Math.floor(elapsed % 60);
-        let minutes = Math.floor(elapsed / 60);
+//Preparations to animate the circle, to update when the song plays
+song.ontimeupdate = () => {
+    let currentTime = song.currentTime;
+    let elapsed = fakeDuration - currentTime;
+    let seconds = Math.floor(elapsed % 60);
+    let minutes = Math.floor(elapsed / 60);
 
-        //Animate the circle details
-        let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
-        outline.style.strokeDashoffset = progress;
-        //Animate the text of the sound timer
-        timeDisplay.textContent = `${minutes}:${seconds}`;
+    //Animate the circle details
+    let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
+    outline.style.strokeDashoffset = progress;
+    //Animate the text of the sound timer
+    timeDisplay.textContent = `${minutes}:${seconds}`;
 
-        //To fix time-display for the sound so that when the song reaches its fakeDuration it pauses & resets
-        if(currentTime >= fakeDuration){
-            song.pause();
-            song.currentTime = 0;
-            play.src = '/assets/svg/play.svg'
-        }
-    };
+    //To fix time-display for the sound so that when the song reaches its fakeDuration it pauses & resets
+    if(currentTime >= fakeDuration){
+        song.pause();
+        song.currentTime = 0;
+        play.src = '/assets/svg/play.svg';
+        video.pause();
+    }
 };
 
 
-app();
+
