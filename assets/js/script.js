3b401bcea793c13ec99dc8ab6d9a105ef7a233ab
Roibéard R Coughlan <roibeard.c@outlook.com>
2021-08-16 10:10:58 +0000

    Tidy indentation of JavaScript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,29 @@
 const app = () => {
-    const song = document.querySelector('.song');
-    const play = document.querySelector('.play');
-    const outline = document.querySelector('.moving-outline circle');
-    const video = document.querySelector('.vid-container video');
+  const song = document.querySelector('.song');
+  const play = document.querySelector('.play');
+  const outline = document.querySelector('.moving-outline circle');
+  const video = document.querySelector('.vid-container video');
 
-    // Select all the Sounds using querySelectorAll
-    const sounds = document.querySelectorAll('.sound-picker button');
-    // Time display
-    const timeDisplay = document.querySelector('.time-display');
-    // Get the lenght of the outline for the sounds to allow annimation
-    const outlineLength = outline.getTotalLength();
-    console.log(outlineLength);
-    //Duration function for the user to chose when the song stops
-    let fakeDuration = 600;
+  // Select all the Sounds using querySelectorAll
+  const sounds = document.querySelectorAll('.sound-picker button');
+  // Time display
+  const timeDisplay = document.querySelector('.time-display');
+  // Get the lenght of the outline for the sounds to allow annimation
+  const outlineLength = outline.getTotalLength();
+  console.log(outlineLength);
+  //Duration function for the user to chose when the song stops
+  let fakeDuration = 600;
 
-outline.style.strokeDasharray = outlineLength;
-outline.style.strokeDashoffset = outlineLength;
+  outline.style.strokeDasharray = outlineLength;
+  outline.style.strokeDashoffset = outlineLength;
 
-//Play sound
+  //Play sound
     play.addEventListener("click", () => {
         checkPlaying(song);
     });
+
+  //Select sound
+  timeSelect
 
 // Create a function specific to stop and play the sounds
         const checkPlaying = song => {
