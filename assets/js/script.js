5fb9735efa3adb30c3fc4202e4316fcc8c0f61f2
Roibéard R Coughlan <roibeard.c@outlook.com>
2021-08-16 09:32:43 +0000

    Add semicolon to line 5 due to 1st JavaScript Validator reslut

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ const app = () => {
     const song = document.querySelector('.song');
     const play = document.querySelector('.play');
     const outline = document.querySelector('.moving-outline circle');
-    const video = document.querySelector('.vid-container video')
+    const video = document.querySelector('.vid-container video');
 
     // Select all the Sounds using querySelectorAll
     const sounds = document.querySelectorAll('.sound-picker button');
